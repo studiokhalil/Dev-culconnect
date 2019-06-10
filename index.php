3be3dc4a4c2b34806a797ab075abd886d258dfec
DmatrixNg <49094365+DmatrixNg@users.noreply.github.com>
2019-06-10 18:35:03 +0100

    update

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php echo "welcome"; ?>
-  </body>
-</html>
+<?php
+
+//use culconnect\Config;
+require_once('router.php');
+// We'll first create a router instance and then register our
+// static routes with the router. Next, we'll try to parse the
+//request url and match it against routes in the collection.
+$router = new Router;
+// Add the static routes
+$id =  $_SERVER["REQUEST_METHOD"];
+
+$router->add([
+  '/' => 'index.html',
+    '/index' => 'index.html',
+    '/signup' => 'signup.html',
+]);
+
+
+$request = $_SERVER['REQUEST_URI'];
+//echo $request;
+if (strlen($request) > 1) {
+  $request = rtrim($request, '/');
+  //  echo $request;
+}
+
+$view = $router->match($request);
+
+require $view;
+?>
